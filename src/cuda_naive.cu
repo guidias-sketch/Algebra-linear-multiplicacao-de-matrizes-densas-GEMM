70cbc903232b1eca9061f3d3496a9980b7be9c5a
guidias-sketch <gui.dias@estudante.ifgoiano.edu.br>
2026-05-26 19:00:19 -0300

    Update cuda_naive.cu

--- src/cuda_naive.cu
+++ src/cuda_naive.cu
@@ -1,1 +1,12 @@
+__global__ void cuda_naive(float* A, float* B, float* C, int N) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
 
+    if (row < N && col < N) {
+        float sum = 0.0f;
+        for (int k = 0; k < N; ++k) {
+            sum += A[row * N + k] * B[k * N + col];
+        }
+        C[row * N + col] = sum;
+    }
+}
