dfd26d2aaf408caf8a6b581690f2fd765c1287aa
guidias-sketch <gui.dias@estudante.ifgoiano.edu.br>
2026-05-26 19:00:00 -0300

    Update matrix_multiply.cpp

--- src/matrix_multiply.cpp
+++ src/matrix_multiply.cpp
@@ -1,1 +1,11 @@
-
+void cpu_sequential(float* A, float* B, float* C, int N) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            float sum = 0.0f;
+            for (int k = 0; k < N; ++k) {
+                sum += A[i * N + k] * B[k * N + j];
+            }
+            C[i * N + j] = sum;
+        }
+    }
+}
